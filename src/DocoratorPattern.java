0e60f430a7b95219278aa59a58add0a7efa3dc95
AkilaGunasekara <cst18022@std.uwu.ac.lk>
2022-11-24 12:19:51 +0530

    Update Decorator Pattern

--- src/DocoratorPattern.java
+++ src/DocoratorPattern.java
@@ -6,6 +6,12 @@ public class DocoratorPattern {
         Pizza Cpizza = new Chicken(Basic);
         System.out.println("Type :"+ Cpizza.getDescription() + "Price :" + Cpizza.getPrice());
 
+        Pizza Cheepizza = new Cheese(Basic);
+        System.out.println("Type : " + Cheepizza.getDescription() + "Price :" + Cheepizza.getPrice());
+
+        Pizza Spizza = new Sausage(Basic);
+        System.out.println("Type : ");
+
 
     }
 }
