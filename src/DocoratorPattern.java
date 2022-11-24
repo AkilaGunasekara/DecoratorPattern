76f9162973f98ccb8c7963d0daffab63e3ac3975
AkilaGunasekara <cst18022@std.uwu.ac.lk>
2022-11-25 01:26:33 +0530

    Update to get user Input

--- src/DocoratorPattern.java
+++ src/DocoratorPattern.java
@@ -1,20 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DocoratorPattern {
     public static void main(String[] args) {
-        Pizza Basic = new BasicPizza();
-        System.out.println("Type :"+ Basic.getDescription() + "Price :" + Basic.getPrice());
+//        Pizza Basic = new BasicPizza();
+//        System.out.println("Type :"+ Basic.getDescription() + "Price :" + Basic.getPrice());
+//
+//        Pizza Cpizza = new Chicken(Basic);
+//        System.out.println("Type :"+ Cpizza.getDescription() + "Price :" + Cpizza.getPrice());
+//
+//        Pizza Cheepizza = new Cheese(Basic);
+//        System.out.println("Type : " + Cheepizza.getDescription() + "Price :" + Cheepizza.getPrice());
+//
+//        Pizza Spizza = new Sausage(Basic);
+//        System.out.println("Type : " + Spizza.getDescription() + "Price :" + Spizza.getPrice());
+//
+//        Pizza Tpizza = new Tomato(Basic);
+//        System.out.println("Type : " + Tpizza.getDescription() + "Price :" + Tpizza.getPrice());
 
-        Pizza Cpizza = new Chicken(Basic);
-        System.out.println("Type :"+ Cpizza.getDescription() + "Price :" + Cpizza.getPrice());
+        ArrayList<String> Arr = new ArrayList<String>();
 
-        Pizza Cheepizza = new Cheese(Basic);
-        System.out.println("Type : " + Cheepizza.getDescription() + "Price :" + Cheepizza.getPrice());
+        System.out.println("Enter Your Choices \n1.Chicken \n2.Cheese \n3.Beef");
+        Scanner sc = new Scanner(System.in);
 
-        Pizza Spizza = new Sausage(Basic);
-        System.out.println("Type : " + Spizza.getDescription() + "Price :" + Spizza.getPrice());
+        sc.nextLine();
 
-        Pizza Tpizza = new Tomato(Basic);
-        System.out.println("Type : " + Tpizza.getDescription() + "Price :" + Tpizza.getPrice());
+        for (int i = 0; i < 3; i++) {
+            Arr.add(sc.nextLine());
+        }
+        Pizza pizza2 = new BasicPizza();
+        for (String i: Arr) {
+            switch (i){
+                case "1":
+                    pizza2 = new Tomato(pizza2);
+                    break;
 
+                case "2":
+                    pizza2 = new Sausage(pizza2);
+                    break;
+                case "3":
+                    pizza2 = new Cheese(pizza2);
+                    break;
+                case "4":
+                    pizza2 = new Chicken(pizza2);
+                    break;
+            }
+
+        }
+        System.out.println(pizza2.getDescription());
+        System.out.println(pizza2.getPrice());
 
     }
+
+
 }
